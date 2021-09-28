6a145806e22ef106650c05af51c658cb07d2aa8f
Saria Houloubi <houloubis96@outlook.com>
2021-09-28 22:41:00 +0400

    GL: Moved folder structure

---
+++ _old/ServerRoot/Controllers/DownloadController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace ServerRoot.Controllers
+{
+    public class DownloadController : Controller
+    {
+
+        [Route("/download/TaskSchedulingSimulator")]
+        public IActionResult TaskSchedulingSimulator() => File("/Assets/Files/Parallel Processors Simulator V2.zip", "application/x-zip-compressed", "Parallel-Processors-Simulator-V2.zip");
+
+        [Route("/download/SimpleAccount")]
+        public IActionResult SimpleAccount() => File("/Assets/Files/SimpleAccount.zip", "application/x-zip-compressed", "SimpleAccount.zip");
+
+
+    }
+}
+   
