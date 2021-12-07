1d22f181bbd22ff3e99f3a63a6afa1b4decf7685
Saria Houloubi <houloubis96@outlook.com>
2021-12-07 18:16:14 +0400

    Fixed bottom nav center

--- Portfolio/Portfolio.Web/Program.cs
+++ Portfolio/Portfolio.Web/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages(options =>
 {
-    options.RootDirectory = "/Views/Pages";
+        options.RootDirectory = "/Views/Pages";
 });
 builder.Services.AddServerSideBlazor();
 
